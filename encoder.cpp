81ea201928a8b2c0c939a9c9fac08c7ca3c33cc0
emmyoj <eojeda@umich.edu>
2018-07-09 11:45:05 -0400

    added position control

--- encoder.cpp
+++ encoder.cpp
@@ -6,49 +6,49 @@ using namespace std;
 
 encoder_struct encoder;
 //update the encoder with two channles and two directions
-/*
+
 void updateEncoders()
 {
-   
-    int MSB = digitalRead(encoder.pin_a);
-    int LSB = digitalRead(encoder.pin_b);
 
-    int encoded = (MSB << 1) | LSB;
-    int sum = (encoder.lastEncoded << 2) | encoded;
+	int MSB = digitalRead(encoder.pin_a);
+	int LSB = digitalRead(encoder.pin_b);
 
-    if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011)
-    	encoder.value++;
-    if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoder.value--;
-    cout << encoder.lastEncoded << " " << encoded << " ";
-    encoder.lastEncoded = encoded;
-    
-    cout << encoder.value << endl;
-    
-  
+	int encoded = (MSB << 1) | LSB;
+	int sum = (encoder.lastEncoded << 2) | encoded;
+
+	if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011)
+		encoder.value++;
+	if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoder.value--;
+	cout << encoder.lastEncoded << " " << encoded << " ";
+	encoder.lastEncoded = encoded;
+
+	cout << encoder.value << endl;
+
+
 
 }
-*/
-//new motor 4535; 4535.2/360 = 50/x
-double temp = 50*360/4535.2; //
-void updateEncoders()
-{
-    static int preTime = clock();
-    static int curTime = clock();
-    encoder.value++;
-    //cout << "encoder " << encoder.value << endl;
-    //cout << encoder.value << endl;
 
-    if(encoder.value % 50 == 0)
-    {
-        curTime = clock();
-        encoder.speed = temp / (curTime - preTime) * CLOCKS_PER_SEC;
-        //encoder.speed = 200;
-				cout << "speed " <<  encoder.speed <<" degree/s" << endl;
-        
-        //cout << (curTime - preTime) << endl;
-        //cout << CLOCKS_PER_SEC << endl;
-        preTime = curTime;
-    }
+//new motor 4535; 4535.2/360 = 50/x
+/*double temp = 50*360/4535.2; //
+	void updateEncoders()
+	{
+	static int preTime = clock();
+	static int curTime = clock();
+	encoder.value++;
+//cout << "encoder " << encoder.value << endl;
+//cout << encoder.value << endl;
+
+if(encoder.value % 50 == 0)
+{
+curTime = clock();
+encoder.speed = temp / (curTime - preTime) * CLOCKS_PER_SEC;
+//encoder.speed = 200;
+cout << "speed " <<  encoder.speed <<" degree/s" << endl;
+
+//cout << (curTime - preTime) << endl;
+//cout << CLOCKS_PER_SEC << endl;
+preTime = curTime;
+}
 
 }
 
@@ -56,39 +56,54 @@ void updateEncoders()
 /*   two channel and both direction
 void setUpEncoder(int a, int b)
 {
-   
-    encoder.pin_a = a;
-    encoder.pin_b = b;
-    encoder.value = 0;
-    encoder.lastEncoded = 0;
 
-    pinMode(encoder.pin_a, INPUT);
-    pinMode(encoder.pin_b, INPUT);
-    pullUpDnControl(encoder.pin_a, PUD_UP);
-    pullUpDnControl(encoder.pin_b, PUD_UP);
-    wiringPiISR(encoder.pin_a,INT_EDGE_BOTH, &updateEncoders);
-    wiringPiISR(encoder.pin_b,INT_EDGE_BOTH, &updateEncoders);
+encoder.pin_a = a;
+encoder.pin_b = b;
+encoder.value = 0;
+encoder.lastEncoded = 0;
+
+pinMode(encoder.pin_a, INPUT);
+pinMode(encoder.pin_b, INPUT);
+pullUpDnControl(encoder.pin_a, PUD_UP);
+pullUpDnControl(encoder.pin_b, PUD_UP);
+wiringPiISR(encoder.pin_a,INT_EDGE_BOTH, &updateEncoders);
+wiringPiISR(encoder.pin_b,INT_EDGE_BOTH, &updateEncoders);
 
 }
 */
 
 //one channel and one direction
-void setUpEncoder(int a, int b)
-{
-   
-    encoder.pin_a = a;
-    encoder.pin_b = b;
-    encoder.value = 0;
-    encoder.lastEncoded = 0;
-    
+/*void setUpEncoder(int a, int b)
+	{
 
-    pinMode(encoder.pin_a, INPUT);
-    //pinMode(encoder.pin_b, INPUT);
-    pullUpDnControl(encoder.pin_a, PUD_UP);
-    //pullUpDnControl(encoder.pin_b, PUD_UP);
-    wiringPiISR(encoder.pin_a,INT_EDGE_RISING, &updateEncoders);
-    //wiringPiISR(encoder.pin_b,INT_EDGE_BOTH, &updateEncoders);
+	encoder.pin_a = a;
+	encoder.pin_b = b;
+	encoder.value = 0;
+	encoder.lastEncoded = 0;
+
+
+	pinMode(encoder.pin_a, INPUT);
+//pinMode(encoder.pin_b, INPUT);
+pullUpDnControl(encoder.pin_a, PUD_UP);
+//pullUpDnControl(encoder.pin_b, PUD_UP);
+wiringPiISR(encoder.pin_a,INT_EDGE_RISING, &updateEncoders);
+//wiringPiISR(encoder.pin_b,INT_EDGE_BOTH, &updateEncoders);
 
 }
+*/
+//two channel and two direction
+void setUpEncoder(int a, int b)
+{
+	encoder.pin_a = a;
+	encoder.pin_b = b;
+	encoder.value = 0;
+	encoder.lastEncoded = 0;
+	encoder.position = 0;
 
-
+	pinMode(encoder.pin_a, INPUT);
+	pinMode(encoder.pin_b, INPUT);
+	pullUPDNControl(encoder.pin_a, PUD_UP);
+	pullUpDNControl(encoder.pin_b, PUD_UP);
+	wiringPiISR(encoder.pin_a, INT_EDGE_BOTH, &updateEncoders);
+	wiringPiISR(encoder.pin_b, INT_EDGE_BOTH, &updateEncoders);
+}
--- mainEncoder.cpp
+++ mainEncoder.cpp
@@ -12,7 +12,7 @@ using namespace std;
 #define channelA 4 //4 in wiringPi = 16 in RasPi. Define as CLK (blue line)
 #define channelB 5 //5 in wiringPi = 18 in RasPi Define as DT (white line)
 
-
+/*
 int main(int argc, char** argv)
 {
 	int pwmPin = 0; // 0 in wiringPi = 11 in RasPi
@@ -54,7 +54,7 @@ int main(int argc, char** argv)
 	/*
 		 double dt, double maxVal, double minVal, double kp, double ki, double kd,double preError = 0
 		 */
-	pid pid_(sampleTime, 100, 0, 0.5, 0.1, 0);
+/*	pid pid_(sampleTime, 100, 0, 0.5, 0.1, 0);
 
 	target_time = exp_time + clock();
 	cout << "target time: " << target_time << "\n";
@@ -77,4 +77,77 @@ int main(int argc, char** argv)
 return 0;
 
 }
+*/
+int main(int argc, char** argv)
+{
+	int pwmPin = 0; // 0 in wiringPi = 11 in RasPi
+	int range = 100; //10000 / frequency. change based on frequency
+	int dirPin = 2; // 2 in wiringPi = 13 in RasPi
+	int pwmVal = 0; // (duty cycle * range)/100
+	//int speed = encoder.speed;
+	double sampleTime = 0.1; // sec? or micro seconds?
+	double startTime = clock();
+	double target_position = 0;//rpm ? 180
+	int exp_time = 0;// user input time experiment will run... without conversion, user input needs to be in microseconds >= 1000000
+	int target_time = 0; //exp_time + clock()
+	double controlTime;
 
+
+	//wiringPiSetupGpio();
+	cout << "start the program" << endl;
+
+	cout << "You have entered " << argc << " arguments:" << "\n";
+	//cout << argv[1][2] << "\n";
+
+	target_position = atoi(argv[1]);
+	//if argc is empty, set speed to default value
+	//if(argc = 0){
+	//	target speed = 30;
+	//	}
+	exp_time = atoi(argv[2])*1000000;
+
+	cout << "experiment time: " << exp_time << "\n";
+	if(wiringPiSetup() < 0){
+		//cout << "fail now" << endl;
+		fprintf(stderr, "Unable to initialize wiringPi:%s\n",strerror(errno));
+		return 1;
+	}
+	setUpEncoder(channelA, channelB);
+	softPwmCreate (pwmPin, pwmVal, range);
+	digitalWrite(dirPin, LOW) ;
+	pinMode(dirPin, OUTPUT) ;
+	
+		 //double dt, double maxVal, double minVal, double kp, double ki, double kd,double preError = 0
+	
+	pid pid_(sampleTime, 15, -15, 0.5, 0.01, 0);
+
+	target_time = exp_time + clock();
+	cout << "target time: " << target_time << "\n";
+	while(clock() < target_time) //while (clock() < target_time)
+	{
+		controlTime = clock() - startTime;
+		//cout << controlTime << endl;
+		if(controlTime >= sampleTime * CLOCKS_PER_SEC)
+		{
+			//cout << "control time " << controlTime / CLOCKS_PER_SEC << endl;
+			startTime += controlTime;
+			pwmVal = pid_.setPWM(target_position, encoder.position);
+			softPwmWrite (pwmPin, abs(pwmVal) + 4);
+			cout << "pwm " << pwmVal << endl;
+			cout << "position " << target_position << " " << encoder.position << endl; //encoder.speed << endl;
+			if(pwmVal > 0)
+			{
+				digitalWrite(dirPin, LOW);
+				cout << "direction: CW" << endl;
+			}
+			else
+			{
+				digitalWrite(dirPin, HIGH);
+				cout << "direction: CCW" << endl;
+			}
+			//if user input = space bar or enter, stop motor 
+		}
+
+	}
+    return 0;
+}
